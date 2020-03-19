c0577b40c0956cdc8b3ec53559186ad17cb47b47
Dajuan Mcdonald <flopwiki@gmail.com>
2020-03-19 19:16:36 -0400

    refactoring update movie component

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,20 +1,75 @@
-import React from 'react';
-import Loader from 'react-loader-spinner';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const UpdateMovie = () => {
-    return (
+export class UpdateMovie extends Component {
+    state = {
+        metascore: 0,
+        title: '',
+        actor: '',
+        director: '',
+        stars: []
 
-    <div>
-        <form>
-            <div
-                onClick={console.log('Updating...')}
-                style={{color: 'white', width: '20rem', height: '20vh', borderRadius: '2px', backgroundColor: 'blue'}}>
-                Update Movie
+
+    }
+
+    handleStars = () => {
+        const {stars} = this.state
+        stars.push(this.state.actor)
+        this.setState({
+            actor: '', stars
+        });
+    }
+
+    handleInput = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleMovieSubmit = () => {
+        const { stars, title, metascore, director } = this.state;
+        const newMovie = { stars, title, metascore, director}
+        const saveMovie = axios.post('http://localhost:3333/api/movies', newMovie)
+        .then(res => {
+            this.props.history.replace('/');
+        })
+        .catch(err => {
+            console.log(err.response.status)
+        })
+    }
+    render() {
+        return (
+            <div>
+                <form>
+                    <input onChange={this.handleInput}
+                        name="title"
+                        type="text"
+                        value={this.state.title}
+                    />
+
+                    <input onChange={this.handleInput}
+                        name="director"
+                        value={this.state.director}
+                        type="text"
+                    />
+
+                    <input onChange={this.handleInput}
+                        name="actor"
+                        type="text"
+                        value={this.state.actor}
+                    />
+
+                    <input onChange={this.handleInput}
+                        name="metascore"
+                        value={this.state.metascore}
+                        type="text"
+                    />
+
+                    
+                </form>
             </div>
-        </form>
-    </div>
-    )
-
+        )
+    }
 }
 
-export default UpdateMovie;
+export default UpdateMovie
